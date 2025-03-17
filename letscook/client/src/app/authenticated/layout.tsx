7dbd54013c31e2401e91ba030f804e3aba2071e8
Adeel Akhani <akhaniadeel@gmail.com>
2025-03-17 13:48:24 -0400

    Dynamic profile card

--- letscook/client/src/app/authenticated/layout.tsx
+++ letscook/client/src/app/authenticated/layout.tsx
@@ -26,6 +26,10 @@ export default async function Layout({
   if (error || !data?.user) {
     redirect('/login')
   }
+  else{
+    console.log(data.user);
+  }
+
 
 
   return (
--- letscook/client/src/app/authenticated/profile/page.tsx
+++ letscook/client/src/app/authenticated/profile/page.tsx
@@ -1,66 +1,61 @@
-"use server"
-import React from "react"
-import { Button } from "@/components/ui/button"
-import { Badge } from "@/components/ui/badge"
-import signOutFromGoogle  from "@/utils/supabase/signOutFromGoogle"
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table"
-import Image from "next/image"
-import "@/styles/globals.css"
-import DynamicTable from "@/components/ui/dynamicTable"
-import ProfileChart from "@/components/ui/profileChart"
-import ProfileCard from "@/components/ui/profileCard"
+"use server";
+import React from "react";
+import { createClientForServer } from "@/utils/supabase/supabaseClient";
+import { redirect } from "next/navigation";
+
+import "@/styles/globals.css";
+import DynamicTable from "@/components/ui/dynamicTable";
+import ProfileChart from "@/components/ui/profileChart";
+import ProfileCard from "@/components/ui/profileCard";
 
 const submissions = [
-    { author: "Haris Khawja", recipe: "Hakka Chow Mein", difficulty: "Medium" },
-    { author: "Sir Williams", recipe: "Clam Chowder", difficulty: "Hard" },
-    { author: "ishowspeed", recipe: "Chicken Nuggets", difficulty: "Easy" },
-]
+  { author: "Haris Khawja", recipe: "Hakka Chow Mein", difficulty: "Medium" },
+  { author: "Sir Williams", recipe: "Clam Chowder", difficulty: "Hard" },
+  { author: "ishowspeed", recipe: "Chicken Nuggets", difficulty: "Easy" },
+];
 
-const user = {
-    username: "Haris Khawja",
-    email: "hariskhawja@gmail.com",
+const chartData = [
+  { month: "January", meals: 0, recipes: 80 },
+  { month: "February", meals: 305, recipes: 200 },
+  { month: "March", meals: 237, recipes: 120 },
+  { month: "April", meals: 73, recipes: 190 },
+  { month: "May", meals: 209, recipes: 130 },
+  { month: "June", meals: 214, recipes: 140 },
+];
+
+export default async function Profile() {
+  const supabase = await createClientForServer();
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/login");
+  }
+
+  const user = {
+    username: data.user.user_metadata.full_name,
+    email: data.user.user_metadata.email,
     points: 42,
     meals_cooked: 15,
     created_recipes: 2,
     rank: 245,
-    profile_pic: "/GoogleIcon.png"
-}
+    profile_pic: data.user.user_metadata.avatar_url,
+  };
+  return (
+    <div className="min-w-screen min-h-screen">
+      {/* Profile Card */}
+      <div className="lg:flex lg:gap-10 flex-col-1 justify-center mx-auto content-center items-center bg-white">
+        <ProfileCard user={user} />
 
-const chartData = [
-    { month: "January", meals: 0, recipes: 80 },
-    { month: "February", meals: 305, recipes: 200 },
-    { month: "March", meals: 237, recipes: 120 },
-    { month: "April", meals: 73, recipes: 190 },
-    { month: "May", meals: 209, recipes: 130 },
-    { month: "June", meals: 214, recipes: 140 },
-]
+        {/* Submissions Chart */}
+        <ProfileChart elements={chartData} />
+      </div>
 
-export default async function Profile() {
-    return (
-        <div className="min-w-screen min-h-screen">
-            
-            {/* Profile Card */}
-            <div className="lg:flex lg:gap-10 flex-col-1 justify-center mx-auto content-center items-center bg-white">
-                <ProfileCard user={user}/>
-
-                {/* Submissions Chart */}
-                <ProfileChart elements={chartData}/>
-            </div>
-
-            {/* Past Submissions */}
-            <div className="py-5 bg-gray-100 flex-col justify-center content-center items-center mx-auto text-black">
-                <h1 className="text-3xl font-bold mb-5 justify-center content-center text-center items-center mx-auto">Past Submissions</h1>
-                <DynamicTable elements={submissions} />
-            </div>
-
-        </div>
-    )
+      {/* Past Submissions */}
+      <div className="py-5 bg-gray-100 flex-col justify-center content-center items-center mx-auto text-black">
+        <h1 className="text-3xl font-bold mb-5 justify-center content-center text-center items-center mx-auto">
+          Past Submissions
+        </h1>
+        <DynamicTable elements={submissions} />
+      </div>
+    </div>
+  );
 }
