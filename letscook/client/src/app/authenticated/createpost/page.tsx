f5c0bdff68b3aef965f32031c6340f7dba6cf960
Adeel Akhani <akhaniadeel@gmail.com>
2025-03-17 14:26:34 -0400

    retesting

--- letscook/client/src/app/authenticated/createpost/page.tsx
+++ letscook/client/src/app/authenticated/createpost/page.tsx
@@ -1,9 +1,6 @@
 "use server";
 import { createClient } from '@supabase/supabase-js'
 
-
-
-
 import RecipeSub from "@/components/ui/recipeSub";
 
 export default async function RecipeSubmissionPage() {
