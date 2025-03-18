41293ba7561c860c114d6f81d9dfb2e38806e01f
Adeel Akhani <akhaniadeel@gmail.com>
2025-03-18 17:33:09 -0400

    backend endpoints set up and new table for auth users

--- letscook/backend/index.js
+++ letscook/backend/index.js
@@ -1,17 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import routes from './routes/recipes.routes.js';
+import routes from './routes/routes.js';
 import cors from "cors";
 
 
+
+
 dotenv.config();
-app.use(cors());
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors());
 app.use(express.json());
 
-app.use("/api/recipes",routes);
+
+app.use("/api",routes);
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+   console.log(`Server is running on http://localhost:${PORT}`);
 });
--- letscook/backend/routes/recipes.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.get("/abcd", async (req, res) => {
-    res.send({ message: "Hello from recipes route" });
-});
-
-export default router;
