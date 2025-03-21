dc38b7a651efbd121bc85024be58c7948e8d50b8
Adeel Akhani <akhaniadeel@gmail.com>
2025-03-20 21:06:26 -0400

    ...

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import routes from './routes/recipes.routes.js';
+import cors from "cors";
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+
+app.use("/api",routes);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ backend/routes/recipes.routes.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
+
+dotenv.config();
+const router = express.Router();
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+router.get("/thisUser", async (req, res) => {
+  res.send({ message: "Hello I am Adeel" });
+});
+
+router.get("/users", async (req, res) => {
+  
+  const { data, error } = await supabase
+        .from("profiles") 
+        .select("*");
+
+    if (error) return res.status(400).json({ error: error.message });
+
+    res.json(data);
+}
+);
+
+
+export default router;
